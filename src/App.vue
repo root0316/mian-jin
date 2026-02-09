99476a1a1a1cf86e209c954ec12d2e78af5c2685
g <18733838822qq.com>
2026-02-09 16:35:41 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <van-button type="primary">主要按钮</van-button>
     <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="default">危险按钮</van-button>
+    <van-button type="info">默认按钮</van-button>
+    <van-button type="info">警告按钮</van-button>
+    <van-button type="info">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
