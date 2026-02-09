7d155a294f7509400ffd6b2dd39a2cabec3118be
qDell <18733838822qq.com>
2026-02-09 11:32:17 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <van-button type="info">信息按钮</van-button>
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <van-button type="warning">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
